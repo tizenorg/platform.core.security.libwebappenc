3b55297503335bc3b73020d729ca0be58842e37c
Dongsun Lee <ds73.lee@samsung.com>
2015-05-26 09:42:38 +0900

    initial version
    Change-Id: Ibb7891bee67bdd82f34187f5da2fc3c4bbfa8fcd
    Signed-off-by: Dongsun Lee <ds73.lee@samsung.com>

---
+++ srcs/crypto_service.h
@@ -0,0 +1,57 @@
+/*
+ *  Copyright (c) 2000 - 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ *
+ * @file        crypto_service.h
+ * @author      Dongsun Lee (ds73.lee@samsung.com)
+ * @version     1.0
+ * @brief       a header for key manupulatation.
+ */
+
+
+
+#ifndef __TIZEN_CORE_WAE_CRYPTO_SERVICE_H
+#define __TIZEN_CORE_WAE_CRYPTO_SERVICE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>
+
+
+int encrypt_app_dek(const unsigned char* rsaPublicKey, size_t pubKeyLen,
+                    const unsigned char* dek, const int dekLen,
+                    unsigned char** encryptedDek, size_t* encryptedDekLen);
+
+int decrypt_app_dek(const unsigned char* rsaPrivateKey, size_t priKeyLen,
+                    const char* priKeyPassword,
+                    const unsigned char* encryptedDek, size_t dencryptedDekLen,
+                    unsigned char** decryptedDek, size_t* decryptedDekLen);
+
+
+int encrypt_aes_cbc(const unsigned char* pKey, size_t keyLen,
+                    const unsigned char* pData, size_t dataLen,
+                    unsigned char** ppEncryptedData, size_t* pEncDataLen);
+
+int decrypt_aes_cbc(const unsigned char* pKey, size_t keyLen,
+                    const unsigned char* pData, size_t dataLen,
+                    unsigned char** ppDecryptedData, size_t* pDecDataLen);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* __TIZEN_CORE_WAE_CRYPTO_SERVICE_H */
+
---
+++ srcs/wae_initializer.c
@@ -0,0 +1,47 @@
+/*
+ *  Copyright (c) 2000 - 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ *
+ * @file        wae_initializer.c
+ * @author      Dongsun Lee (ds73.lee@samsung.com)
+ * @version     1.0
+ * @brief       tool for importing APP DEKs during booting
+ */
+
+#include "key_handler.h"
+#include "web_app_enc.h"
+#include "wae_log.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    int ret = WAE_ERROR_NONE;
+    int reload = WAE_FALSE;
+
+    if(argc == 2 && strcmp(argv[1], "--reload")==0) {
+        reload = WAE_TRUE;
+    }
+
+    ret = load_preloaded_app_deks(reload);
+    if(ret == WAE_ERROR_NONE) {
+        printf("WAE INITIALIZER was finished successfully.\n");
+        WAE_SLOGI("WAE INITIALIZER was finished successfully.");
+        return 0;
+    }else {
+        printf("WAE INITIALIZER was finished with error. ret=%d\n", ret);
+        WAE_SLOGE("WAE INITIALIZER was finished with error. ret=%d", ret);
+        return -1;
+    }
+}
